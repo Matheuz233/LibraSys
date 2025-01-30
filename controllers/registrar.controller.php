ddda5407e8fc7b345106f868e77a157265910b80
Matheus Augusto <138679799+Matheuz233@users.noreply.github.com>
2025-01-29 22:59:40 -0300

    feat: :hammer: Implementação da Classe de Validação para Registro de Usuários

--- controllers/registrar.controller.php
+++ controllers/registrar.controller.php
@@ -1,40 +1,22 @@
 <?php
 
+require 'Validacao.php';
+
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-  $validacoes = [];
-  $nome = $_POST['nome'];
-  $email = $_POST['email'];
-  $senha = $_POST['senha'];
 
-  if (strlen($nome) == 0) {
-    $validacoes[] = 'O nome é obrigatório';
-  }
+  $validacao = Validacao::validar([
+    'nome' => ['required'],
+    'email' => ['required', 'email'],
+    'senha' => ['required', 'min:8', 'max:30']
+  ], $_POST);
 
-  if (strlen($email) == 0) {
-    $validacoes[] = 'O email é obrigatório';
-  }
+  if ($validacao->naoPassou()) {
 
-  if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-    $validacoes[] = 'O email é obrigatório';
-  }
-
-  if (strlen($senha) == 0) {
-    $validacoes[] = 'A senha é obrigatória';
-  }
-
-  if (strlen($senha) < 8 || strlen($senha) > 30) {
-    $validacoes[] = 'A senha precisa ter entre 8 e 30 caracteres';
-  }
-
-  if (sizeof($validacoes) > 0) {
-
-    $_SESSION['validacoes'] = $validacoes;
 
     header('location: /login');
 
     exit();
   }
-
 
   $database->query(
     query: "insert into usuarios ( nome, email, senha) values (:nome, :email, :senha)",
